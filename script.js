b16c6f5377b03ab7029fc5c8a8166c455c563438
yret1 <135514649+yret1@users.noreply.github.com>
2023-10-30 13:46:47 +0100

    Trying to rotate the rocket according to movement

--- script.js
+++ script.js
@@ -1,32 +1,18 @@
+
+
 let bodyElement = document.querySelector('body')
-bodyElement.addEventListener('mousemove', getMousePosition, false)
+bodyElement.addEventListener('mousemove', function(event){
+    let x = event.clientX;
+    let y = event.clientY;
 
+    let centerX = window.innerHeight / 2;
+    let centerY = window.innerHeight / 2;
+    let radianAngle = Math.atan2( y - centerY, x - centerX);
+    let degreeAngle = radianAngle * (180/Math.PI);
 
-var xDirection = "";
-var yDirection = "";
+    let cursor = document.getElementsByClassName("cursor");
+    cursor[0].style.transform = `rotate(${degreeAngle}deg)`
+    cursor[0].style.left = x + 'px';
+    cursor[0].style.top = y + 'px';
+})
 
-var oldX ="";
-var oldY ="";
-
-
-
-function getMousePosition(xy){
-
-    if(oldX < xy.pageX){
-        xDirection = "right;"
-    }else{
-        xDirection ="left";
-    }
-
-    if(oldY < xy.pageY){
-        yDirection = "down";
-    }else{
-        yDirection = "up";
-    }
-
-    oldX = xy.pageX
-    oldY = xy.pageY
-    return[xDirection,yDirection]
-}
-
-console.log(getMousePosition())
