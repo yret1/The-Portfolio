632fcb82bb8665134683558cbe12a6a88a04c6a5
yret1 <135514649+yret1@users.noreply.github.com>
2023-10-31 12:58:01 +0100

    Figured out tracking for the lil rocket

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 
 
 let bodyElement = document.querySelector('body')
-bodyElement.addEventListener('mousemove', function(event){
-    let x = event.clientX;
-    let y = event.clientY;
+bodyElement.addEventListener('mousemove', updatePos)
 
-    let centerX = window.innerHeight / 2;
-    let centerY = window.innerHeight / 2;
+let x = 0;
+let y = 0;
+
+let prevX = 0;
+let prevY = 0;
+
+
+function updatePos(ev){
+    
+    
+      prevX = x;
+      prevY = y;
+
+    x = ev.clientX
+    y = ev.clientY
+
+    let centerX = prevX;
+    let centerY = prevY;
     let radianAngle = Math.atan2( y - centerY, x - centerX);
     let degreeAngle = radianAngle * (180/Math.PI);
+    let degreeAngleCompensated = (degreeAngle + 90) % 360;
 
     let cursor = document.getElementsByClassName("cursor");
-    cursor[0].style.transform = `rotate(${degreeAngle}deg)`
+    cursor[0].style.transform = `rotate(${degreeAngleCompensated}deg)`
     cursor[0].style.left = x + 'px';
     cursor[0].style.top = y + 'px';
-})
+}
 
